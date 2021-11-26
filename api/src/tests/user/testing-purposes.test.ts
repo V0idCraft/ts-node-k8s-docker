3fe4cc9d96cd007cbef61781519983aa338620b4
Jose Gil <josegil9807@gmail.com>
2021-11-26 15:54:34 -0400

    Remove test case

--- api/src/tests/user/testing-purposes.test.ts
+++ api/src/tests/user/testing-purposes.test.ts
@@ -8,13 +8,4 @@ describe("Test cases for testing-purposes file", () => {
     expect(total).toBe(20);
 
   });
-
-  it("Should return the sum of two numbers twice", () => {
-
-    const total = sum(10,10);
-    const total2 = sum(2,2);
-    expect(total).toBe(20);
-    expect(total2).toBe(4);
-
-  });
 });
