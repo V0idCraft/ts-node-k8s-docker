0d3881277e2afb5dc9123fcabf6aceff0f4d0fcb
Jose Gil <josegil9807@gmail.com>
2021-11-27 10:36:08 -0400

    Skaffold added for dev purposes and modify folder structure for infrastructre

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,7 +2,6 @@ import { app } from "./app";
 
 const port = process.env.APP_PORT || 3000;
 
-
 app.listen(port, () => {
-  console.log("Server running at port", port);
+  console.log(`✔️  Server running at port`, port);
 });
